0f3d945e649fb6983d738225341138f237fee6e7
Volodymyr Semchii <semchiy14@gmail.com>
2021-11-01 19:41:59 +0200

    added two more tests

--- tests.py
+++ tests.py
@@ -8,5 +8,14 @@ class FirstTaskTest (TestCase):
         self.assertEqual(first_task('abcd efgh'), 'dcba hgfe')
 
 
+    def test_two(self):
+        with self.assertRaises(AttributeError):
+            first_task(0)
+
+
+    def test_three(self):
+        first_task('a1bcd efg!h')
+        self.assertTrue(first_task('d1cba hgf!e'))
+
 if __name__ == '__main__':
     main()
